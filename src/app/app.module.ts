5299bcf9080995d662efe76fe0e7455167fe4dac
Julio Saráuz <juliosarauz1i@gmail.com>
2020-10-25 12:22:19 -0500

    first commi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+//IMPORTACIONES DE PRIMENG
+import {MegaMenuModule} from 'primeng/megamenu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PickListModule} from 'primeng/picklist';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MegaMenuModule,
+    PickListModule,
+    InputTextareaModule,
+    FormsModule,
+    ButtonModule,
+    CardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
